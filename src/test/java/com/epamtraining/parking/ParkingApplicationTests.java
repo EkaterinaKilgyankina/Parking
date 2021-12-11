67959cb2a74fa63a232b1fa613cba3a80cf7e463
Ekaterina Kilgiankina <ekaterina_kilgiankina@epam.com>
2021-12-11 14:38:02 +0000

    Update ParkingApplicationTests.java

--- src/test/java/com/epamtraining/parking/ParkingApplicationTests.java
+++ src/test/java/com/epamtraining/parking/ParkingApplicationTests.java
@@ -35,36 +35,7 @@ class ParkingApplicationTests {
 
     @Test
     void contextLoads() {
-        final CarEntity car = new CarEntity();
-        car.setNumber("123123");
-        final UserEntity user1 = new UserEntity();
-        user1.setEmail("email@email.com");
-        user1.setPassword("pwd");
-        user1.setCars(Collections.singletonList(car));
-        car.setUser(user1);
-        userRepository.save(user1);
-        spotRepositoty.save(new SpotEntity());
-
-        try {
-            final UserEntity user = userRepository.findAll().get(0);
-            final CarEntity carEntity = user.getCars().get(0);
-            final SpotEntity spot = spotRepositoty.findById(1L).get();
-            final BookingEntity bookingEntity = new BookingEntity();
-            bookingEntity.setCarEntity(carEntity);
-            bookingEntity.setSpotEntity(spot);
-            bookingEntity.setBookingFrom(LocalDateTime.now());
-            bookingEntity.setBookingTo(LocalDateTime.now().plusMinutes(30));
-            spot.setBookingEntity(bookingEntity);
-            carEntity.setBookingEntity(bookingEntity);
-//carRepository.save(carEntity)
-            spot.setBookingEntity(bookingEntity);
-            bookingRepository.saveAndFlush(bookingEntity);
-            spotRepositoty.saveAndFlush(spot);
-        } catch (Exception e) {
-            System.out.println("");
-        }
-
-        System.out.printf("asd");
+        
     }
 
 }

e872c2542fe94eb565a7d690ad3be32e110f5609
mariesemenyuk <mariasemenyuk0211@gmail.com>
2021-12-15 10:12:45 +0300

    additional changes for security

--- src/main/java/com/epamtraining/parking/contoller/RegistrationController.java
+++ src/main/java/com/epamtraining/parking/contoller/RegistrationController.java
@@ -1,17 +1,14 @@
 package com.epamtraining.parking.contoller;
 
 import com.epamtraining.parking.domain.entity.UserEntity;
-import com.epamtraining.parking.model.UserRequest;
 import com.epamtraining.parking.repository.UserRepository;
 import com.epamtraining.parking.services.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @RestController
-@RequestMapping("/registration")
+@RequestMapping("/registration/")
 public class RegistrationController {
     @Autowired
     private UserServiceImpl userService;
--- src/main/java/com/epamtraining/parking/contoller/UserController.java
+++ src/main/java/com/epamtraining/parking/contoller/UserController.java
@@ -1,6 +1,5 @@
 package com.epamtraining.parking.contoller;
 
-import com.epamtraining.parking.domain.entity.RoleEntity;
 import com.epamtraining.parking.domain.entity.UserEntity;
 import com.epamtraining.parking.model.ChangeRoleRequest;
 import com.epamtraining.parking.services.UserService;
@@ -8,38 +7,24 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/users/")
 public class UserController {
     private UserService userService;
 
 
     @GetMapping
-    @RolesAllowed("{role_admin}")
     public List<UserEntity> getAll() {
 
         return userService.getAll();
     }
 
-
-    @GetMapping("/{email}")
-    public UserEntity getUser(@PathVariable String email) {
-        UserEntity user = userService.getUser(email);
-      /*  if(user == null) {
-            throw new UsernameNotFoundException(email);
-        }*/
-        return user;
-    }
-
     @PostMapping("/edit/")
-    @RolesAllowed("{role_admin}")
     public ResponseEntity changeUserRole (@RequestBody ChangeRoleRequest role) {
         return ResponseEntity.ok(userService.changeUserRole(role));
     }
 
 }
-

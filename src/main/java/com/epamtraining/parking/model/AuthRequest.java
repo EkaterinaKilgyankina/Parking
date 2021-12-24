4b825a10bdc7b20e7f6e29f9a75f393bdbabf881
Katherin <kilgyankina.ekaterina@gmail.com>
2021-12-24 06:55:42 +0300

    ActionsWithBookingsWithOneUser

--- src/main/java/com/epamtraining/parking/model/AuthRequest.java
+++ src/main/java/com/epamtraining/parking/model/AuthRequest.java
@@ -1,7 +1,17 @@
 package com.epamtraining.parking.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class AuthRequest {
+    @NotNull (message = "email is mandatory")
+    @Email(message = "should be email format")
     private String name;
+    @NotNull (message = "password is mandatory")
+    @Size(min = 5, message = "password length must be more than five")
+    @Pattern(regexp = "^(?=.*?[#?!@$%^&*-])(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z]).{5,}", message = "password must contain letters (upper and lower case), digits and special symbols")
     private String password;
 
     public String getName() {
